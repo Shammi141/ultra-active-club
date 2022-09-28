e1f82cd1ce68279582617af1db5a52feb5dfc013
Shammi141 <jannatulshammi181@gmail.com>
2022-09-29 04:43:00 +0600

    Author's info showed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { faSchool } from '@fortawesome/free-solid-svg-icons';
+import { faSchool, faPerson } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import React from 'react';
@@ -7,9 +7,18 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='text-start ms-2'>
-            <div className='d-flex header-style'>
-                <FontAwesomeIcon className='icon-style' icon={faSchool}></FontAwesomeIcon>
-                <h2 className='text-primary my-3'>For First Learner - FFL</h2>
+            <div className='d-flex developer-info'>
+                <div className='d-flex header-style text-center'>
+                    <FontAwesomeIcon className='icon-style' icon={faSchool}></FontAwesomeIcon>
+                    <h2 className='text-primary my-3'>For First Learner - FFL</h2>
+                </div>
+                <div className='d-flex header-style'>
+                    <FontAwesomeIcon className='profile-icon' icon={faPerson}></FontAwesomeIcon>
+                    <div className=''>
+                        <p className='my-0 fw-semibold'>Shammi Akter</p>
+                        <small>Junior Developer</small>
+                    </div>
+                </div>
             </div>
             <h5 className='mb-5'>Click to Grab Your Courses</h5>
         </div>
