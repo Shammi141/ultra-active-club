e8e18ad71ff039d24d53d64227e4638e0332eb1c
Shammi141 <jannatulshammi181@gmail.com>
2022-09-29 01:59:31 +0600

    course info added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './Course.css'
 
 const Course = (props) => {
-    const {picture, time, title, about} = props.course;
+    const {picture, time, age, title, about} = props.course;
 
     return (
-        <div className='course-img'>
+        <div className='course text-start'>
             <img src= {picture} alt="" />
+            <div className='mx-2'>
+                <p className='course-name mb-2'>{title}</p>
+                <p>{about}</p>
+                <p className='mb-0'>For Age: {age}</p>
+                <p className='mb-1'>Time Required: {time}</p>
+            </div>
+            <button className='btn btn-primary ms-2 btn-cart'>Add To List</button>
         </div>
     );
 };
